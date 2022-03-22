c586454a7b8335b29f211aed38fd4bcc5e65de14
rossebell <Rosy24dr@outlook.com>
2022-03-22 17:38:02 -0400

    trying to figure out how to store the robot & dino result from the for loops to use dino and robot's turn and also in the battle method

--- battlefield.py
+++ battlefield.py
@@ -1,16 +1,20 @@
 from fleet import Fleet
 from herd import Herd
-# from robot import Robot
+from robot import Robot
 
-class Battleship:
+
+
+class Battlefield:
 
     def __init__(self):
         self.fleet = Fleet()
         self.herd = Herd()
-        # self.robot = Robot()
+        self.attack = Robot('Loki')
         self.fleet.create_fleet()
         self.herd.create_herd()
-        # self.robot.attack()
+        self.robot_result_list = []
+        self.dino_result_list = []
+
     
     def run_game(self):
         pass
@@ -19,35 +23,37 @@ class Battleship:
         print('Welcome to the Robots vs Dinosours battle!!')
 
     def battle(self):
-        # self.herd.robot_one.attack(self.dinosour_one)
-        # self.herd.robot_two.attack(self.dinosour_two)
-        pass
-    
+        print(f"{self.robot_result_list}'s uses {self.robot.weapon} and {self.attack} {self.dino_result_list}")
+
     def dino_turn(self,dinosour):
-        pass
+        print(f"It is {dinosour}'s turn.")
 
     def robo_turn(self,robot):
-        print(f"It is the {robot}'s turn.")
+        print(f"It is {robot}'s turn.")
         
     def show_dino_opponent_options(self):
+        self.dino_result_list  = []
+        print('These are the dinosours:')
         for dino in self.herd.dinosours:
             print(dino.name)
+            self.dino_result_list.append(dino)
 
     def show_robo_opponent_options(self):
+        self.robot_result_list = []
+        print('These are the robots:')
         for robot in self.fleet.robots:
             print(robot.name)
+            self.robot_result_list.append[robot]
 
     def display_winners(self):
         pass
 
-welcome_msg = Battleship()
-print(welcome_msg.display_welcome())
+welcome_msg = Battlefield()
+robot = Battlefield()
+dino = Battlefield()
 
-robot_opponents = Battleship()
-print(robot_opponents.show_robo_opponent_options())
-
-dino_opponents = Battleship()
-print(dino_opponents.show_dino_opponent_options())
-
-the_robots_turn = Battleship()
-print(the_robots_turn.robo_turn(dino_opponents.show_robo_opponent_options[0].name))
+welcome_msg.display_welcome()
+dino.show_dino_opponent_options()
+robot.show_robo_opponent_options()
+dino.dino_turn(dino.herd.dinosours[0].name)
+robot.robo_turn(robot.fleet.robots[0].name)
