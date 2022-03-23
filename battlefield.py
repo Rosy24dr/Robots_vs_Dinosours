0b8ae448a2a51e454f5422fa9f189c9df8d537a6
rossebell <Rosy24dr@outlook.com>
2022-03-23 01:50:54 -0400

    made changes to the battlefield file  after Meghan's review.

--- battlefield.py
+++ battlefield.py
@@ -9,13 +9,9 @@ class Battlefield:
     def __init__(self):
         self.fleet = Fleet()
         self.herd = Herd()
-        self.attack = Robot('Loki')
         self.fleet.create_fleet()
         self.herd.create_herd()
-        self.robot_result_list = []
-        self.dino_result_list = []
-
-    
+        
     def run_game(self):
         pass
 
@@ -23,7 +19,12 @@ class Battlefield:
         print('Welcome to the Robots vs Dinosours battle!!')
 
     def battle(self):
-        print(f"{self.robot_result_list}'s uses {self.robot.weapon} and {self.robot.attack()} {self.dino_result_list}")
+            self.herd.dinosours[0].attack(self.fleet.robots[0])
+            print('The dino attacked the robot')
+            self.fleet.robots.
+            self.fleet.robots[0].attack(self.herd.dinosours[0])
+        
+        
 
     def dino_turn(self,dinosour):
         print(f"It is {dinosour}'s turn.")
@@ -32,28 +33,26 @@ class Battlefield:
         print(f"It is {robot}'s turn.")
         
     def show_dino_opponent_options(self):
-        self.dino_result_list  = []
         print('These are the dinosours:')
         for dino in self.herd.dinosours:
             print(dino.name)
-            self.dino_result_list.append(dino)
+          
 
     def show_robo_opponent_options(self):
-        self.robot_result_list = []
         print('These are the robots:')
         for robot in self.fleet.robots:
             print(robot.name)
-            self.robot_result_list.append(robot)
+            
+            
 
     def display_winners(self):
         pass
 
-welcome_msg = Battlefield()
-robot = Battlefield()
-dino = Battlefield()
+my_battlefield = Battlefield()
 
-welcome_msg.display_welcome()
-dino.show_dino_opponent_options()
-robot.show_robo_opponent_options()
-dino.dino_turn(dino.herd.dinosours[0].name)
-robot.robo_turn(robot.fleet.robots[0].name)
+my_battlefield.display_welcome()
+my_battlefield.show_dino_opponent_options()
+my_battlefield.show_robo_opponent_options()
+my_battlefield.dino_turn(my_battlefield.herd.dinosours[0].name)
+my_battlefield.robo_turn(my_battlefield.fleet.robots[0].name)
+my_battlefield.battle()
